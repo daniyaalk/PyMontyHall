f9c98f7977487dfaeeffa00b28a5cd323098c4c8
Daniyaal Khan <khandaniyaal2000@gmail.com>
2018-10-06 12:20:30 +0300

    Completed Simulation output

--- montyhall.py
+++ montyhall.py
@@ -7,7 +7,22 @@ switchwin = 0
 stickwin = 0
 
 while i < n:
+
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("Simulation number: " + str(i+1))
+
     winningdoor = random.randint(1,3)
     doorchosen = random.randint(1,3)
-    print(str(winningdoor)+"=>"+str(doorchosen) + "WIN" if (winningdoor==doorchosen) else "LOSS")
+
+    # If doorchosen == winningdoor, stickwin will be the winning choise, otherwise switchwin
+    if doorchosen == winningdoor:
+        stickwin += 1
+        print("Sticking to initial option is the winning choice")
+    else:
+        switchwin += 1
+        print("Switching initial option is the winning choice")
+
+    print("Score so far: " if (i!=n-1) else "Final Score")
+    print("Times won by sticking to initial choice: " + str(stickwin))
+    print("Times won by switching choice: " + str(switchwin))
     i+=1
