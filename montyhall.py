2d1889c7b9a700fec567fdbc7da4ba3d3c0ec45d
Daniyaal Khan <khandaniyaal2000@gmail.com>
2018-10-07 12:05:03 +0300

    Improved graphing

--- montyhall.py
+++ montyhall.py
@@ -1,4 +1,4 @@
-import random, pylab, numpy as np
+import random, pylab, numpy as np, argparse as ap, time, os
 
 n = int(input("Enter number of simulations to run: "))
 a = int(input("Enter door count: "))
@@ -9,10 +9,22 @@ stickwin = 0
 stickwinlist = []
 switchwinlist = []
 
+def clear():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+
+
+#Argument Parser
+parser = ap.ArgumentParser()
+parser.add_argument("-v", action="store_true", help="Display results so far after each trial", default=False)
+args, leftovers = parser.parse_known_args()
+
 while i < n:
 
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    print("Simulation number: " + str(i+1))
+    if args.v: print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    if args.v: print("Simulation number: " + str(i+1))
 
     winningdoor = random.randint(1,a)
     doorchosen = random.randint(1,a)
@@ -20,30 +32,33 @@ while i < n:
     # If doorchosen == winningdoor, stickwin will be the winning choise, otherwise switchwin
     if doorchosen == winningdoor:
         stickwin += 1
-        #pylab.scatter(stickwin, i, c='r', label='Stick')
-        print("Sticking to initial option is the winning choice")
+        pylab.scatter(i, stickwin, c='r')
+        if args.v: print("Sticking to initial option is the winning choice")
     else:
         switchwin += 1
-        #pylab.scatter(switchwin, i, c='g', label='Switch')
-        print("Switching initial option is the winning choice")
+        pylab.scatter(i, switchwin, c='g')
+        if args.v: print("Switching initial option is the winning choice")
 
     stickwinlist.append(stickwin)
     switchwinlist.append(switchwin)
 
-    print("Score so far: " if (i!=n-1) else "Final Score")
-    print("Times won by sticking to initial choice: " + str(stickwin))
-    print("Times won by switching choice: " + str(switchwin))
-    #pylab.pause(0.05)
+    if args.v:
+        print("Score so far: " if (i!=n-1) else "Final Score")
+        print("Times won by sticking to initial choice: " + str(stickwin))
+        print("Times won by switching choice: " + str(switchwin))
+    else:
+        clear()
+        print(str((i*100)/n)+"%")
 
     i+=1
 
 pylab.title("Comparison of switch vs stick strategy for Monty Hall Problem over " + str(n) + " trials")
-pylab.plot(np.arange(0,n), stickwinlist, 'b', label='Stick')
+pylab.plot(np.arange(0,n), stickwinlist, 'r', label='Stick')
 pylab.plot(np.arange(0,n), switchwinlist, 'g', label='Switch')
 pylab.legend(loc='best')
 pylab.ylabel("Number of wins")
 pylab.xlabel("Number of trials")
-print("=============================================")
+if args.v: print("=============================================")
 result = ((switchwin)/n)
 expectation = ((a-1)/a)
 print("Simulated probability of winning by switching: " + str(result))
